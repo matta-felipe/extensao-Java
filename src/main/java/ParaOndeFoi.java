f216a095f0d29d44466c13f272f41db36de5fec0
fematta <fmattacloud@gmail.com>
2024-09-29 22:22:16 -0300

    classe Coordenada - atributos e metodos

--- src/main/java/ParaOndeFoi.java
+++ src/main/java/ParaOndeFoi.java
@@ -81,4 +81,28 @@ class Empresa {
 
 
 
-class Coordenada {}
+class Coordenada {
+    private double lat;
+    private double lon;
+
+    public Coordenada(double lat, double lon) {
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }   
+
+    public double getLon() {
+        return lon;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordenada{" +
+                "lat=" + lat +
+                ", lon=" + lon +
+                '}';
+    }
+}
